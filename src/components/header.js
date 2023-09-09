cb656640ab158033d405ba628e9f573c13a3fa43
prathmesh4912 <prathmeshpawaru@gmail.com>
2023-09-10 01:32:09 +0530

    Menu Button Added

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,17 @@ export default function Header() {
   const[temp , settemp] = useState(false) ;
 
   const [hl , sethl] = useState("12vh") ;
-
+  const [hn , sethn]=useState("12vh") ;
   function closeMobileNavOnResize() {
     if (window.innerWidth < 768) {
       setsham(true);
     } else {
       setsham(false);
+    }
+
+    if(window.innerWidth < 430)
+    {
+      sethn("8vh")
     }
   }
   window.onresize = closeMobileNavOnResize;
@@ -20,7 +25,7 @@ const tog = ()=>{
   if(temp)
   {
     settemp(false) ;
-    sethl("12vh") ;
+    sethl(hn) ;
   }else{
     settemp(true) ;
     sethl("35vh") ;
@@ -29,7 +34,7 @@ const tog = ()=>{
 
 const tfal = ()=>{
   settemp(false) ;
-  sethl("12vh") ;
+  sethl(hn) ;
 }
 
 useEffect(()=>{
@@ -54,7 +59,8 @@ useEffect(()=>{
         <div className="dh1 " style={{height : hl}}>
 
         <div className="hw1 text-center">
-          <svg
+        {/* {!temp ?  */}
+        <svg
             stroke="currentColor"
             fill="currentColor"
             stroke-width="0"
@@ -66,15 +72,21 @@ useEffect(()=>{
             style={{ color: "white" }}
             onClick={tog}
           >
-            <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
+           <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
           </svg>
+        {/* //   :
+        //   <svg             onClick={tog}
+        //   xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-x-lg" viewBox="0 0 16 16">
+        //   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+        // </svg> 
+        // } */}
 
           {temp ? <div className="container d-flex flex-column justify-content-center">
-            <div className="mt-3" onClick={tfal}  > <a href="#bg">Home</a></div>
-            <div className="my-1"  onClick={tfal} > <a href="#projects">Projects</a></div>
-            <div className="my-1" onClick={tfal} > <a href="#skills">Skills</a></div>
-            <div className="my-1" onClick={tfal} > <a href="#hobbies">Hobbies</a></div>
-            <div className="my-1" onClick={tfal} > <a href="#home">About Me</a>{" "}</div>
+            <div className=""  > <a onClick={tfal}  href="#bg">Home</a></div>
+            <div className=""  > <a onClick={tfal} href="#projects">Projects</a></div>
+            <div className="" > <a onClick={tfal} href="#skills">Skills</a></div>
+            <div className="" > <a onClick={tfal} href="#hobbies">Hobbies</a></div>
+            <div className="" > <a onClick={tfal} href="#home">About Me</a>{" "}</div>
           </div> : <></>}
         </div>
         </div>
