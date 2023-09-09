409472153783fab85ddb6c25b2f48bd7d4675cfc
prathmesh4912 <prathmeshpawaru@gmail.com>
2023-09-10 02:19:46 +0530

    Links Updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -34,7 +34,7 @@ export default function home(props) {
                 className="mb-4 d-flex justify-content-around ms-5 my-3 "
                 style={{ width: "10vw" }}
               >
-                <a href="http://google.com">
+                <a href="https://www.linkedin.com/in/prathmesh-pawar-17b929229/" target="_blank">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -46,10 +46,10 @@ export default function home(props) {
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                   </svg>
                 </a>
-                <a href="http://instagram.com" target="_blank" rel="external">
+                <a className="mx-1" href="mailto: prathmeshpawaru@gmail.com" target="_blank" rel="noreferrer">
                   <img src={Gmail} width={"30px"} alt="" />
                 </a>
-                <a href="http://github.com">
+                <a href="https://github.com/Prathmesh-Pawar" target="_blank">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -64,7 +64,7 @@ export default function home(props) {
               </div>
               <span>
                 <a
-                  href="https://drive.google.com/file/d/1iuMTP7Wd7EBrqT2awbY8gt4BAVetrD-6/view"
+                  href="https://drive.google.com/file/d/1eKwymLOdPnuAEzvhU0aHLS4IWIv7rnxk/view?usp=sharing"
                   rel="external"
                   target="_blank"
                   className=" btn btn-sm bsm  m-3 me-5 btn-dark "
