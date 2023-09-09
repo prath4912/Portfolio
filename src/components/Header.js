4e1f71625523ed157893a8383fad293555e856ee
prathmesh4912 <prathmeshpawaru@gmail.com>
2023-09-10 01:38:20 +0530

    Close Button for menu added

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ useEffect(()=>{
         <div className="dh1 " style={{height : hl}}>
 
         <div className="hw1 text-center">
-        {/* {!temp ?  */}
+        {!temp ? 
         <svg
             stroke="currentColor"
             fill="currentColor"
@@ -74,19 +74,19 @@ useEffect(()=>{
           >
            <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
           </svg>
-        {/* //   :
-        //   <svg             onClick={tog}
-        //   xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-x-lg" viewBox="0 0 16 16">
-        //   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-        // </svg> 
-        // } */}
+          :
+          <svg             onClick={tog}
+          xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-x-lg" viewBox="0 0 16 16">
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+        </svg> 
+        }
 
           {temp ? <div className="container d-flex flex-column justify-content-center">
-            <div className=""  > <a onClick={tfal}  href="#bg">Home</a></div>
-            <div className=""  > <a onClick={tfal} href="#projects">Projects</a></div>
-            <div className="" > <a onClick={tfal} href="#skills">Skills</a></div>
-            <div className="" > <a onClick={tfal} href="#hobbies">Hobbies</a></div>
-            <div className="" > <a onClick={tfal} href="#home">About Me</a>{" "}</div>
+            <div className="mt-3"  > <a onClick={tfal}  href="#bg">Home</a></div>
+            <div className="my-1"  > <a onClick={tfal} href="#projects">Projects</a></div>
+            <div className="my-1" > <a onClick={tfal} href="#skills">Skills</a></div>
+            <div className="my-1" > <a onClick={tfal} href="#hobbies">Hobbies</a></div>
+            <div className="my-1" > <a onClick={tfal} href="#home">About Me</a>{" "}</div>
           </div> : <></>}
         </div>
         </div>
