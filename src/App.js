e602dcad4ca45b9de7ea84e6adee98ea7684f7d5
prathmesh4912 <prathmeshpawaru@gmail.com>
2024-08-02 22:48:56 +0530

    Footer import updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Header from './components/Header';
 import About from './pages/About' ;
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
-import Footer from './components/Footer';
 import Hobbies from './pages/Hobbies' ;
 import Home from './pages/Home';
+import Footer from "./components/Footer"
 
 function App() {
   return (
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,6 @@ import "./footer.css";
 
 export default function Footer() {
   return (
-    <>
       <div className="bg-dark-subtle p-3">Developed By Prathmesh</div>
-    </>
   );
 }
